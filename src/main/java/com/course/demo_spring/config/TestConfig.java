6692878b9823bf946fe904b444ac44f56e701bf4
Leonardo Raupp <leonardoraupprodrigues@hotmail.com>
2024-12-26 18:53:11 -0300

    Enum OrderStatus.

--- src/main/java/com/course/demo_spring/config/TestConfig.java
+++ src/main/java/com/course/demo_spring/config/TestConfig.java
@@ -2,6 +2,7 @@ package com.course.demo_spring.config;
 
 import com.course.demo_spring.entities.Order;
 import com.course.demo_spring.entities.User;
+import com.course.demo_spring.entities.enums.OrderStatus;
 import com.course.demo_spring.repositories.OrderRepository;
 import com.course.demo_spring.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -29,9 +30,9 @@ public class TestConfig implements CommandLineRunner {
 
         userRepository.saveAll(Arrays.asList(user1, user2));
 
-        Order order1 = new Order(null, Instant.parse("2024-12-26T17:30:22Z"), user1); // ISO 8601 pattern
-        Order order2 = new Order(null, Instant.parse("2024-12-22T09:10:34Z"), user2);
-        Order order3 = new Order(null, Instant.parse("2024-12-26T17:31:30Z"), user1);
+        Order order1 = new Order(null, Instant.parse("2024-12-26T17:30:22Z"), OrderStatus.PAID, user1); // ISO 8601 pattern
+        Order order2 = new Order(null, Instant.parse("2024-12-22T09:10:34Z"), OrderStatus.DELIVERED, user2);
+        Order order3 = new Order(null, Instant.parse("2024-12-26T17:31:30Z"), OrderStatus.PAID, user1);
 
         orderRepository.saveAll(Arrays.asList(order1, order2, order3));
     }
