ee11a508ce685e3ffbf8954a653cf6367e47d6ee
Leonardo Raupp <leonardoraupprodrigues@hotmail.com>
2025-01-02 19:22:04 -0300

    crud methods User entity.

--- src/main/java/com/course/demo_spring/resources/UserResource.java
+++ src/main/java/com/course/demo_spring/resources/UserResource.java
@@ -2,13 +2,13 @@ package com.course.demo_spring.resources;
 
 import com.course.demo_spring.entities.User;
 import com.course.demo_spring.services.UserService;
+import jakarta.servlet.Servlet;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
@@ -29,5 +29,25 @@ public class UserResource {
     public ResponseEntity<Optional<User>> findById(@PathVariable Long id) {
         Optional<User> user = userService.findById(id);
         return ResponseEntity.ok(user);
+    }
+
+    @PostMapping
+    public ResponseEntity<User> insert(@RequestBody User user) {
+        user = userService.insert(user);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+                .buildAndExpand(user.getId()).toUri();
+        return ResponseEntity.created(uri).body(user);
+    }
+
+    @PutMapping(value = "{id}")
+    public ResponseEntity<User> update(@PathVariable Long id, @RequestBody User user) {
+        user = userService.update(id, user);
+        return ResponseEntity.ok().body(user);
+    }
+
+    @DeleteMapping(value = "{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        userService.delete(id);
+        return ResponseEntity.noContent().build();
     }
 }
--- src/main/java/com/course/demo_spring/services/UserService.java
+++ src/main/java/com/course/demo_spring/services/UserService.java
@@ -22,4 +22,24 @@ public class UserService {
     public Optional<User> findById(Long id) {
         return userRepository.findById(id);
     }
+
+    public User insert(User obj) {
+        return userRepository.save(obj);
+    }
+
+    public User update(Long id, User obj) {
+        User entity = userRepository.getReferenceById(id);
+        updateData(entity, obj);
+        return userRepository.save(entity);
+    }
+
+    private void updateData(User entity, User user) {
+        entity.setName(user.getName());
+        entity.setEmail(user.getEmail());
+        entity.setPhone(user.getPhone());
+    }
+
+    public void delete(Long id) {
+        userRepository.deleteById(id);
+    }
 }
